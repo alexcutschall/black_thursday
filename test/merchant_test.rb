a782bad600997f2b607babe6ce1a98ac100fd592
alexcutschall <alexander.cutschall@gmail.com>
2018-02-18 12:41:32 -0700

    Adding the merchant_test along with the merchant.rb halfway through the iteration 0 video

---
+++ test/merchant_test.rb
@@ -0,0 +1,24 @@
+require './test/test_helper'
+require './lib/merchant'
+
+class MerchantTest < Minitest::Test
+
+  def test_it_can_be_instantiated
+    merchant = Merchant.new({:id =>5, :name => "Turing School"})
+    assert_instance_of Merchant, merchant
+  end
+
+  def test_it_has_attributes_id_and_name
+    merchant = Merchant.new({:id =>5, :name => "Turing School"})
+
+    assert_equal 5, merchant.id
+    assert_equal "Turing School", merchant.name
+  end
+
+  def test_it_can_have_different_attributes
+    merchant = Merchant.new({:id => 6, :name => "Codecademy"})
+
+    assert_equal 6, merchant.id
+    assert_equal "Codecademy", merchant.name
+  end
+end
