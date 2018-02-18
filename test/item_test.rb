bcbf34da49a1fd80b80fde6898e010172664a48d
alexcutschall <alexander.cutschall@gmail.com>
2018-02-18 16:07:02 -0700

    Added an item class along with classes checking the attributes.

---
+++ test/item_test.rb
@@ -0,0 +1,27 @@
+require 'bigdecimal'
+require_relative 'test_helper'
+require_relative '../lib/item'
+
+class ItemTest < Minitest::Test
+
+  def setup
+    @item = item = Item.new({:name  => "Pencil",
+                     :description   => "You can use it to write things",
+                     :unit_price    => BigDecimal.new(10.99,4),
+                     :created_at    => Time.now,
+                     :updated_at    => Time.now,
+                     })
+  end
+
+  def test_class_can_be_instantiated
+    assert_instance_of Item, @item
+  end
+
+  def test_class_has_attributes
+    assert_equal "Pencil", @item.name
+    assert_equal "You can use it to write things", @item.description
+  end
+
+  
+
+end
